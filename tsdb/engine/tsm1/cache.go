cf34709eead56d9db140210748243bf06499f058
rw <me@rwinslow.com>
2016-07-26 18:20:21 -0700

    nice

--- tsdb/engine/tsm1/cache.go
+++ tsdb/engine/tsm1/cache.go
@@ -168,9 +168,9 @@ func NewCache(maxSize uint64, path string) *Cache {
 func (c *Cache) destroyStore() {
 	for _, os := range c.internedOwnedStrings {
 		delete(c.internedOwnedStrings, os)
-		c.arena.Dec(os)
+		//c.arena.Dec(os)
 		delete(c.store, os)
-		c.arena.Dec(os)
+		//c.arena.Dec(os)
 	}
 	if len(c.store) != 0 {
 		panic("nonempty c.store on destroyStore")
