f04e3d8b548d5e5e2d49ff8cd2a1d42e87c7b3a8
rw <me@rwinslow.com>
2016-07-28 13:05:27 -0700

    wip

--- tsdb/engine/tsm1/slab_pools_test.go
+++ tsdb/engine/tsm1/slab_pools_test.go
@@ -50,10 +50,20 @@ func TestByteSliceSlabPool(t *testing.T) {
 
 func TestStringSlabPool(t *testing.T) {
 	p := NewStringSlabPool()
-	str, buf := p.Get(1)
-	buf[0] = 'x'
-	if str != "x" {
-		t.Fatal("bad string write")
+	s0, b0 := p.Get(1)
+	b0[0] = 'x'
+	if s0 != "x" {
+		t.Fatal("bad string write s0")
 	}
+
+	s1, b1 := p.Get(1)
+	b1[0] = 'y'
+	if s1 != "y" {
+		t.Fatal("bad string write s1")
+	}
+	if s0 != "x" {
+		t.Fatal("bad overwrite s0")
+	}
+
 
 }
