1722828bce773f86759bb6a0853098a1f2a24538
Palak Bhojani <palak@influxdata.com>
2019-01-10 17:07:28 -0800

    wip

--- ui/src/onboarding/components/configureStep/Scraping.tsx
+++ ui/src/onboarding/components/configureStep/Scraping.tsx
@@ -2,15 +2,35 @@
 import React, {PureComponent} from 'react'
 
 // Components
-// import {Form} from 'src/clockface'
-// import  MultipleInput from 'src/onboarding/components/configureStep/streaming/MultipleInput'
+import {Form, Input, Dropdown} from 'src/clockface'
 
 interface Props {}
 
 class Scraping extends PureComponent<Props> {
   public render() {
-    return <div />
+    return (
+      <Form>
+        <Form.Element label="Interval">
+          <Input widthPixels={200} />
+        </Form.Element>
+        <Form.Element label="Bucket">
+          {/* <Dropdown
+            selectedID={'1'}
+            onChange={this.handleOnClick}
+            widthPixels={200}
+          >
+            <Dropdown.Item key={1} value={1} id={'1'}>
+              defbuck
+            </Dropdown.Item>
+            ))}
+          </Dropdown> */}
+        </Form.Element>
+      </Form>
+    )
   }
 }
+// private handleOnClick = (value: any) => {
+//   console.log(value)
+// }
 
 export default Scraping
