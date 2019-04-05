0d1be25db8932a08e86a7931d5883613bf8d904f
Adam <adam@influxdata.com>
2019-04-05 11:44:57 -0400

    working on benchmarks

--- benchmarking/series_count_tagkeys.sh
+++ benchmarking/series_count_tagkeys.sh
@@ -1,30 +1,19 @@
 STEPS=$1
 shift
-TOKEN=$1
+TOKEN=5CJjFZBVRme8YRCEsSbonzgmz-Y50HsggB8QVPrUI9_zno_Zz9YzbZynZ94BWI6Dkz2CC5p63agcOgLRQtQLlw==
 shift
 query=$*
 
-make > /dev/null && echo "make successful"
 NPOINTS=1000000
 KEYSCNT="10"
 
 NVALUES=1
 for i in {2..6}
 do
-    let NVALUES=(10**i)
-    KEYSCNT="$KEYSCNT,10"
-    let NPOINTS=1000000/NVALUES
-
-    KEYSTR="$KEYSCNT"
-    echo "./bin/darwin/influxd generate simple --t $KEYSTR --p $NPOINTS --clean all --bucket my-bucket --org my-org"
-    ./bin/darwin/influxd generate simple --t $KEYSTR --p $NPOINTS --clean all --bucket my-bucket --org my-org
-
-./bin/darwin/influxd >/dev/null &
-INFLUX_PID=$!
-sleep 5
-
-hyperfine -r 10 --shell bash -u millisecond --export-markdown series_size.md "curl http://localhost:9999/api/v2/query?org=my-org -XPOST -sS -H 'Authorization: Token $TOKEN' -H 'accept:application/csv' -H 'content-type:application/vnd.flux' -d '$query'" 
+    measurement=$i"t10v1000000p"
+    echo $measurement
+    hyperfine -r 10 --shell bash -u millisecond --export-markdown series_size.md "curl http://localhost:8086/api/v2/query?org=my-org -XPOST -sS -H 'Authorization: Token $TOKEN' -H 'accept:application/csv' -H 'content-type:application/vnd.flux' -d 'from(bucket:\"my-bucket\") |> range(start:-30d)|> filter(fn: (r) => r._measurement == $measurement)'" 
 
 
-kill $INFLUX_PID
+#kill $INFLUX_PID
 done
