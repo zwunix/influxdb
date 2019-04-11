9aae4d4ed7cf49d1a698e8f5325d76b96bf290f8
Adam <adam@influxdata.com>
2019-04-11 15:37:35 -0400

    add scripts to reproduce data generation

---
+++ benchmarking/generate_for_1x.sh
@@ -0,0 +1,3 @@
+./influx_tools gen-init -config influxconf.yaml -db=db -rp=rp
+
+./influx_tools gen-exec -config influxconf.yaml -schema benchmarking/generate_all_cases.toml -db=db -rp=rpenerate_all_cases.toml -db=db -rp=rp
---
+++ benchmarking/generate_for_2x.sh
@@ -0,0 +1,1 @@
+./bin/darwin/influxd generate --clean all --bucket db --org my-org benchmarking/generate_all_cases.toml
